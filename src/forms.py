ed548ae6b4c59c9ac00b80c1ca83bf11ffa3af0e
Timon <timon@riegerx.de>
2024-11-06 18:47:21 +0100

    import form options from npoint and user permissions

--- src/forms.py
+++ src/forms.py
@@ -1,6 +1,14 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField, SelectMultipleField
 from wtforms.validators import DataRequired, URL, Length, Optional
+import requests
+
+NPOINT = "https://api.npoint.io/1ba4f8744068b81b8070"
+medium_choices = requests.get(url=NPOINT).json()["medium"]
+category_choices = requests.get(url=NPOINT).json()["category"]
+topic_choices = requests.get(url=NPOINT).json()["topic"]
+tags_choices = requests.get(url=NPOINT).json()["tags"]
+
 
 class RessourceForm(FlaskForm):
     name = StringField("Name", validators=[DataRequired(), Length(max=10)])
@@ -8,44 +16,25 @@ class RessourceForm(FlaskForm):
     description = StringField("Description", validators=[Optional()])
     medium = SelectField(
         "Medium", 
-        choices=[
-            ('Article', 'Article'),
-            ('Website', 'Website'),
-            ('Study/Paper', 'Study/Paper'),
-            ('Tool', 'Tool'),
-        ],
+        choices=medium_choices,
         validators=[DataRequired()]
     )
 
     category = SelectField(
         "Category", 
-        choices=[
-            ('Programming', 'Programming'),
-            ('Design', 'Design'),
-            ('Machine Learning', 'Machine Learning'),
-            ('Trading', 'Trading'),
-            ('Maths', 'Maths'),
-        ],
+        choices=category_choices,
         validators=[DataRequired()]
     )
 
     topic = SelectField(
         "Topic", 
-        choices=[
-            ('Security', 'Security'),
-            ('Frameworks', 'Frameworks'),
-            ('Documentation', 'Documentation'),
-            ('Repositories', 'Repositories'),
-        ],
+        choices=topic_choices,
         validators=[DataRequired()]
     )
 
     tags = SelectMultipleField(
         "Tags", 
-        choices=[
-            ('Must Have', 'Must Have'),
-            ('Optional', 'Optional'),
-        ],
+        choices=tags_choices,
         validators=[Optional()]
     )
     submit = SubmitField(label="Save")
