49be74849def78474b01bcca5fcdc91db21f56d4
timonrieger <github.com.kindle922@passmail.net>
2025-01-19 21:35:02 +0100

    refactor: rebrand to arkivr

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# ressource-library
-Library for resources of any kind
+# arkivr
+A public archiving app for various resources, websites or articles, organised in a single place.
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,8 @@ module.exports = {
   content: [
     "./templates/**/*.html",
     "./static/src/**/*.js",
-    "./node_modules/flowbite/**/*.js",
   ],
   theme: {
     extend: {},
   },
-  plugins: [require("flowbite/plugin")],
 };
