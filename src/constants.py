13112926379343b31b7a2919d59a3e8e3ea74383
timonrieger <mail@timonrieger.de>
2025-10-28 14:10:46 +0100

    fix data path

--- src/constants.py
+++ src/constants.py
@@ -5,7 +5,7 @@ import os
 
 dotenv.load_dotenv()
 
-with open("static/data/data.json", "r") as f:
+with open(os.path.join(os.path.dirname(os.path.dirname(__file__)), '..', 'static', 'data', 'data.json'), 'r') as f:
     data = json.load(f)
 
 MEDIUM_CHOICES = data["medium"]
